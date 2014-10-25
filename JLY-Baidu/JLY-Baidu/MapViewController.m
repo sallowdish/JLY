3a2c86f84ef9c2e8d9210af0e69a8819b49c526f
sallowdish <zhrud21@gmail.com>
2014-10-25 04:25:09 -0700

    Update
    1. get the current location when start
    2. add annotation view as a pin
    3. display annotation title and user location

--- JLY-Baidu/JLY-Baidu/MapViewController.m
+++ JLY-Baidu/JLY-Baidu/MapViewController.m
@@ -9,8 +9,10 @@
 #import "MapViewController.h"
 
 
-@interface MapViewController () <BMKMapViewDelegate>
+@interface MapViewController () <BMKMapViewDelegate,BMKLocationServiceDelegate>
 @property (strong,nonatomic) BMKMapView* mapView;
+@property (strong,nonatomic) BMKLocationService* locService;
+@property (strong,nonatomic) id<BMKAnnotation> myLocationPin;
 @end
 
 @implementation MapViewController
@@ -19,20 +21,75 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     
-    [super viewDidLoad];
+    //inilialize map
     _mapView = [[BMKMapView alloc]initWithFrame:CGRectMake(0, 0, 320, 480)];
     self.view = _mapView;
+    
+    
 }
 
 - (void)viewWillAppear:(BOOL)animated
 {
     [_mapView viewWillAppear];
     _mapView.delegate = self; // 此处记得不用的时候需要置nil，否则影响内存的释放
+    
+    //get current location
+    //初始化BMKLocationService
+    _locService = [[BMKLocationService alloc]init];
+    _locService.delegate = self;
+    //启动LocationService
+    [_locService startUserLocationService];
 }
 -(void)viewWillDisappear:(BOOL)animated
 {
     [_mapView viewWillDisappear];
     _mapView.delegate = nil; // 不用时，置nil
+    
+    _locService.delegate=nil;
+}
+
+- (void)didUpdateUserLocation:(BMKUserLocation *)userLocation
+{
+    NSLog(@"didUpdateUserLocation lat %f,long %f",userLocation.location.coordinate.latitude,userLocation.location.coordinate.longitude);
+
+    [self addPinOnMap:userLocation];
+    
+//    _mapView.showsUserLocation = YES;//显示定位图层
+//    [_mapView updateLocationData:userLocation];
+    [_locService stopUserLocationService];
+    
+}
+-(void)addPinOnMap:(BMKUserLocation*) userLocation{
+    if (!_myLocationPin) {
+        BMKPointAnnotation* newAno=[[BMKPointAnnotation alloc] init];
+        newAno.title=@"You are here";
+        newAno.subtitle=[NSString stringWithFormat:@"%f,%f",userLocation.location.coordinate.latitude,userLocation.location.coordinate.longitude];
+        _myLocationPin=(id<BMKAnnotation>)newAno;
+    }
+    _myLocationPin.coordinate=userLocation.location.coordinate;
+    [_mapView addAnnotation:_myLocationPin];
+}
+
+-(BMKAnnotationView *)mapView:(BMKMapView *)mapView viewForAnnotation:(id<BMKAnnotation>)annotation{
+    if ([annotation isKindOfClass:[BMKPointAnnotation class]]){
+        BMKPinAnnotationView *newAnnotationView = [[BMKPinAnnotationView alloc] initWithAnnotation:annotation reuseIdentifier:@"myAnnotation"];
+        
+        newAnnotationView.pinColor = BMKPinAnnotationColorPurple;
+        
+        newAnnotationView.animatesDrop = YES;// 设置该标注点动画显示
+        
+        newAnnotationView.canShowCallout = YES;
+        
+        return newAnnotationView;
+        
+    }
+    
+    return nil;
+    
+}
+
+- (void)didFailToLocateUserWithError:(NSError *)error{
+    NSLog(@"Fail to get current location, %@", [error localizedDescription]);
 }
 
 - (void)didReceiveMemoryWarning {
